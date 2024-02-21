744ae1bfc7b9879156860fb4a2da30973b698a99
ephraim <ephraim@localhost>
2024-02-21 15:43:45 -0600

    commit

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,28 @@
 package studio4;
 
 import java.awt.Color;
+import java.util.Scanner;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		
+		Color blue = new Color(0, 0, 255);
+		StdDraw.setPenColor(blue);
+		StdDraw.filledRectangle(0.2, .5, 0.2, 0.5);
+		Color red = new Color(255, 0, 0);
+		StdDraw.setPenColor(red);
+		StdDraw.filledRectangle(0.7, .25, 0.3, 0.25);
+		double[]x = new double[] {0.1,0.2,0.3};
+		double[]y = new double[] {0.45,0.6,0.45};
+		double[]x2 = new double[] {0.1,0.2,0.3};
+		double[]y2 = new double[] {0.55,0.4,0.55};
+		Color white = new Color(255,255,255);
+		StdDraw.setPenColor(white);
+		StdDraw.filledPolygon(x,y);
+		StdDraw.setPenColor(white);
+		StdDraw.filledPolygon(x2,y2);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,45 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		System.out.println(shape);
+		boolean filled = in.nextBoolean();
+		double[] x = new double[] {in.nextDouble(),in.nextDouble(),in.nextDouble()};
+		double[] y = new double[] {in.nextDouble(),in.nextDouble(),in.nextDouble()};
 		
+
+//		double doublex = in.nextDouble();
+//		double doubley = in.nextDouble();
+//		double halfW = in.nextDouble();
+//		double halfH = in.nextDouble();
+
+		StdDraw.setPenColor(red,green,blue); 
+		if (filled = true) {
+			if (shape.equals("ellipse")) {
+				StdDraw.filledEllipse(x[0], x[1], x[2], y[0]);
+			}
+			if (shape.equals("rectangle")) {
+				StdDraw.filledRectangle(x[0], x[1], x[2], y[0]);
+			}
+			if (shape.equals("triangle")) {
+				StdDraw.filledPolygon(x,y);
+			}
+		}
+		else {
+			if (shape.equals("ellipse")) {
+				StdDraw.ellipse(x[0], x[1], x[2], y[0]);
+			}
+			if (shape.equals("rectangle")) {
+				StdDraw.rectangle(x[0], x[1], x[2], y[0]);
+			}
+			if (shape.equals("triangle")) {
+				StdDraw.polygon(x,y);
+			}
+		}
+
 	}
 }
